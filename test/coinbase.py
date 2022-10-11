54ec53d0b1d6eba0228513bd5d2dec22b443c6ab
Andres Rangel <andres.rangel@featuremine.com>
2022-10-11 14:45:41 -0400

    coinbase test fix

--- test/coinbase.py
+++ test/coinbase.py
@@ -30,8 +30,7 @@ import shutil
 import argparse
 
 src_dir = os.path.dirname(os.path.realpath(__file__))
-bulldozer_dir = os.path.join(src_dir, '..', '..', 'bulldozer')
-ytp_file = os.path.join(bulldozer_dir, 'tests', 'data', 'coinbase_bulldozer.base.ytp')
+ytp_file = os.path.join(src_dir, 'data', 'coinbase_bulldozer.base.ytp')
 book_base_file = os.path.join(src_dir, 'data', 'coinbase_bulldozer.base.csv')
 book_test_file = os.path.join(src_dir, 'data', 'coinbase_bulldozer.test.csv')
 
