55ed20b6762dc071ea30f36c18666a02b6f9e956
Andres Rangel <andres.rangel@featuremine.com>
2022-10-25 11:27:17 -0400

    pandas play fix

--- python/py_utils.hpp
+++ python/py_utils.hpp
@@ -835,6 +835,14 @@ PyObject *result_as_pandas(const fm_frame_t *frame,
         *(char *)PyArray_GETPTR1((PyArrayObject *)array, item) =
             char(*(BOOL *)fm_frame_get_cptr1(frame, i, item));
       }
+    } else if (fm_type_base_enum(decl) == FM_TYPE_DECIMAL128) {
+      for (int item = 0; item < f_dims[0]; ++item) {
+        auto *val = ExtractorBaseTypeDecimal128::py_new(*(DECIMAL128 *)fm_frame_get_cptr1(frame, i, item));
+        PyArray_SETITEM((PyArrayObject *)array,
+                        (char*)PyArray_GETPTR1((PyArrayObject *)array, item),
+                        val);
+        Py_XDECREF(val);
+      }
     } else {
       memcpy(PyArray_GETPTR1((PyArrayObject *)array, 0),
              fm_frame_get_cptr1(frame, i, 0), elem_size * f_dims[0]);
