7206074fc24dae256abef81c3f6b1caf9df9a3ce
Andres Rangel <andres.rangel@featuremine.com>
2022-11-09 09:07:44 -0500

    untracked files added

---
+++ python/include/extractor/python/decimal128.h
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+        COPYRIGHT (c) 2017 by Featuremine Corporation.
+        This software has been provided pursuant to a License Agreement
+        containing restrictions on its use.  This software contains
+        valuable trade secrets and proprietary information of
+        Featuremine Corporation and is protected by law.  It may not be
+        copied or distributed in any form or medium, disclosed to third
+        parties, reverse engineered or used in any manner not provided
+        for in said License Agreement except with the prior written
+        authorization from Featuremine Corporation.
+
+ *****************************************************************************/
+
+/**
+ * @file decimal128.h
+ * @date 9 Nov 2022
+ * @brief File contains C Python api for Decimal128 Type
+ *
+ * This file contains C Python api for Decimal128 Type
+ * @see http://www.featuremine.com
+ */
+
+#ifndef __FM_PY_DECIMAL128_H__
+#define __FM_PY_DECIMAL128_H__
+
+#include <Python.h>
+
+#include "fmc/platform.h"
+#include "fmc/decimal128.h"
+
+FMMODFUNC bool Decimal128_Check(PyObject *obj);
+
+FMMODFUNC fmc_decimal128_t Decimal128_val(PyObject *obj);
+
+FMMODFUNC PyObject *Decimal128_new(fmc_decimal128_t obj);
+
+#endif // __FM_PY_DECIMAL128_H__
