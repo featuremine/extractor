2146724b8cca2de6a1e676527e91af016acc07d9
Andres Rangel <andres.rangel@featuremine.com>
2023-08-17 18:24:22 -0300

    fixes after discussion

--- include/extractor/api.h
+++ include/extractor/api.h
@@ -35,39 +35,16 @@ typedef struct fm_type_sys_t fm_type_sys_t;
 typedef struct fm_comp_graph_t fm_comp_graph_t;
 typedef struct fm_stream_ctx_t fm_stream_ctx_t;
 typedef struct fm_comp_t fm_comp_t;
+typedef struct fm_exec_ctx fm_exec_ctx_t;
 typedef void *fm_frame_clbck_cl;
-
-typedef void (*compsys_unaryfunc) (fm_comp_sys_t *);
-typedef fm_type_sys_t* (*compsys_typesys) (fm_comp_sys_t *);
-typedef fm_comp_graph_t* (*compsys_graph) (fm_comp_sys_t *);
-typedef bool (*compsys_typeadd) (fm_comp_sys_t *, const fm_comp_def_t *def);
-
-typedef fm_stream_ctx_t* (*compsys_streamctx) (fm_comp_sys_t *, fm_comp_graph_t *);
-typedef fm_stream_ctx_t* (*compsys_streamctx_rec) (fm_comp_sys_t *, fm_comp_graph_t *, fm_writer, void *);
-
-typedef char* (*compsys_errormsg) (fm_comp_sys_t *);
-
-fm_comp_t * (*graph_compfind) (fm_comp_graph_t *, const char*)
-
-bool (*streamctx_procone) (fm_stream_ctx_t *, fmc_time64_t);
-fmc_time64_t (*streamctx_nexttime) (fm_stream_ctx_t *);
-bool (*streamctx_iserror) (fm_stream_ctx_t *);
-const char * (*streamctx_errormsg) (fm_stream_ctx_t *);
 
 typedef void (*fm_frame_clbck_p)(const fm_frame_t *, fm_frame_clbck_cl,
                                  fm_call_ctx_t *);
 
-fm_result_ref_t *(*comp_getref)(fm_comp_t *) {
-void (*comp_clbckset)(fm_comp_t *, fm_frame_clbck_p, fm_frame_clbck_cl) {
-
 typedef const struct fm_type_decl *fm_type_decl_cp;
 typedef struct fm_frame fm_frame_t;
 
-fm_type_decl_cp (*comp_resulttype)(const fm_comp_t *);
-unsigned (*typeframe_nfields)(fm_type_decl_cp);
-const char * (*typeframe_fieldname)(fm_type_decl_cp, int);
-const void * (*frame_getcptr1) (const fm_frame_t *, fm_field_t, int);
-
+typedef int fm_field_t;
 typedef void *fm_comp_def_cl;
 typedef fm_ctx_def_t *(*fm_comp_def_gen)(fm_comp_sys_t *sys, fm_comp_def_cl,
                                          unsigned, fm_type_decl_cp[],
@@ -82,48 +59,53 @@ typedef struct {
 
 struct extractor_api_v1 {
   // Clean up system
-  compsys_unaryfunc cleanup;
+  void (*comp_sys_cleanup) (fm_comp_sys_t *);
   // Get type sys
-  compsys_typesys type_sys;
+  fm_type_sys_t* (*type_sys_get) (fm_comp_sys_t *);
   // Get graph
-  compsys_graph graph;
+  fm_comp_graph_t* (*comp_graph_get) (fm_comp_sys_t *);
   // Get stream context
-  compsys_streamctx stream_ctx;
+  fm_stream_ctx_t* (*stream_ctx_get) (fm_comp_sys_t *, fm_comp_graph_t *);
   // Get recorded stream context
-  compsys_streamctx_rec stream_ctx_recorded;
+  fm_stream_ctx_t* (*stream_ctx_recorded) (fm_comp_sys_t *, fm_comp_graph_t *, fm_writer, void *);
   // Get replayed stream context
-  compsys_streamctx_rec stream_ctx_replayed;
+  fm_stream_ctx_t* (*stream_ctx_replayed) (fm_comp_sys_t *, fm_comp_graph_t *, fm_writer, void *);
   // Get error msg
-  compsys_errormsg error_msg;
+  char* (*comp_sys_error_msg) (fm_comp_sys_t *);
   // Add computation definition to computational system
-  compsys_typeadd compsys_type_add;
+  bool (*comp_type_add) (fm_comp_sys_t *, const fm_comp_def_t *def);
 
   // Find computation node in graph
-  graph_compfind comp_find;
+  fm_comp_t * (*comp_find) (fm_comp_graph_t *, const char*)
 
   // Stream context, process one
-  streamctx_procone stream_ctx_proc_one;
+  bool (*stream_ctx_proc_one) (fm_stream_ctx_t *, fmc_time64_t);
   // Stream context, next time
-  streamctx_nexttime stream_ctx_next_time;
-  // Stream context, is error
-  streamctx_iserror stream_ctx_is_error;
-  // Stream context, error msg
-  streamctx_errormsg stream_ctx_error_msg;
+  fmc_time64_t (*stream_ctx_next_time) (fm_stream_ctx_t *);
+
+  // Execution context, is error
+  bool (*exec_ctx_is_error) (fm_exec_ctx_t *);
+  // Execution context, error msg
+  const char * (*exec_ctx_error_msg) (fm_exec_ctx_t *);
 
   // Obtain result frame reference from computation
-  comp_getref comp_get_ref;
-
+  fm_result_ref_t *(*result_ref_get)(fm_comp_t *);
   // Set callback to computation
-  comp_clbckset comp_clbck_set;
+  void (*comp_clbck_set)(fm_comp_t *, fm_frame_clbck_p,
+                         fm_frame_clbck_cl);
 
   // Get computation result type
-  comp_resulttype comp_result_type;
+  fm_type_decl_cp (*comp_result_type)(const fm_comp_t *);
   // Get type number of fields
-  typeframe_nfields typeframe_nfields;
+  unsigned (*type_frame_nfields)(fm_type_decl_cp);
   // Get type field name
-  typeframe_fieldname typeframe_field_name;
+  const char *(*type_frame_field_name)(fm_type_decl_cp, int);
   // Get frame data
-  frame_getcptr1 frame_get_cptr1;
+  const void *(*frame_get_cptr1)(const fm_frame_t *, fm_field_t, int);
+  // Get frame field
+  fm_field_t (*frame_field)(const fm_frame_t *, const char *);
+  // Get result reference data
+  fm_frame_t *(*data_get)(fm_result_ref_t *);
 };
 
 // function that you can call to return the actual sequence api structure
