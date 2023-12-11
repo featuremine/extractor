7a14cf00531676624421a32e4f42c364f0195caf
Maxim Trokhimtchouk <max@featuremine.com>
2023-12-11 12:42:40 -0500

    fixes for strings

--- src/mp_record.cpp
+++ src/mp_record.cpp
@@ -79,6 +79,7 @@ bool fm_comp_mp_record_call_stream_init(fm_frame_t *result, size_t args,
 
   FILE *file = nullptr;
   auto pipe = fmc::begins_with_pipe(info->file);
+  string name_str = pipe.second;
   auto *name = pipe.second.c_str();
   if (pipe.first) {
     fmc_error_t *err = nullptr;
